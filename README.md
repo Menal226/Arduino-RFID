6acb135538225923ea8a64c97e01cdb6c28a7aad
Menal226 <michal.menal226@gmail.com>
2023-06-28 20:11:13 +0200

    Added the "A" command
    User can now add IDs using the "A" command

--- README.md
+++ README.md
@@ -2,9 +2,22 @@
 
 This is a project I did to learn how to communicate between two arduino boards.
 
-When an RFID tag is placed near the scanner the client arduino sends it's ID to the server board. Server checks if the ID is allowed to enter in which case 1 is sent to the client board and green LED flashes. If the ID isn't allowed 2 is sent and red LED flashes.
+When an RFID tag is placed near the scanner the client arduino sends it's ID to the server board using the pins 3 and 5. The server checks if the ID is allowed to enter in which case 1 is sent to the client board and green LED flashes. If the ID isn't allowed 2 is sent and red LED flashes. (Allowed to enter means that the ID is on the allowedlist and it is currently the within the aproved time interval.
 
-Stuff to implement:
- 1. Timed entrances = ID is only allowed in predefined timelimits
- 2. Storing data in EEPROM = I want to store and recover the ID and times to and from EEPROM 
- 3. Adding/setting/editting/deleting IDs using UART = Have the Server accept request over UART
+Allowed IDs are stored in the servers EEPROM into which new one can be added/from which user can remove IDs by issuing command listed bellow.
+
+**Commands for server**
+Commands must be input with the "no new line" opion selected in the serial monitor
+ 1. A {ID in decimal}
+	- This command adds a device with the specified ID to the list of allowed devices
+	- Carefull: This sets the device's entry time as allways
+	- Example: S 205 206 207 208
+ 2. R {ID in decimal}
+	- This command removes the device with the specified ID from the list of allowed devices
+	- This also clears all the setup time intervals asociated with ID (including last entry time)
+	- Example: R 205 206 207 208
+ 3. T {ID in decimal} {start of the allowed time in format hh:mm} {end of the allowed time in format hh:mm}
+	- This command sets a time interval in which the ID is allowed to enter
+	- Example: T 205 206 207 208 16:00 18:00
+ 4. S
+	- This command lists out all the currently allowed IDs together with their allowed times and last entry time
--- Server/Server.ino
+++ Server/Server.ino
@@ -1,4 +1,6 @@
+
 //COM4
+#include <EEPROM.h>
 #include <Dictionary.h>
 #include <DictionaryDeclarations.h>
 #include <RTClib.h>
@@ -7,18 +9,31 @@
 #define RX_PIN 5 //The blue cable
 #define TX_PIN 3 //The white cable
 
-//Creates dicts into which the times will be stored
-Dictionary *minEntryTime = new Dictionary();
-Dictionary *maxEntryTime = new Dictionary();
-Dictionary *lastEntryTime = new Dictionary();
+/*
+EEPROM Memory structure
+Max 10 ID najednou
+[x] = ID[0]
+[x+1] = ID[1]
+[x+2] = ID[2]
+[x+3] = ID[3]
+[x+4] = minHour
+[x+5] = minMin
+[x+6] = maxHour
+[x+7] = maxMin
+[x+8] = lastHour
+[x+9] = lastMin
+...
+
+[100] = Je 1 místo plné? 1:0
+[101] = Je 2 místo plné? 1:0
+...
+*/
 
 //Creates an instance of the RTC
 RTC_DS1307 rtc;
 //Creates communication between the arduinos
 SoftwareSerial ardvComm (RX_PIN, TX_PIN);
 char buffer[128];
-//Array of allowed IDs
-char allowedIDs[10][15];
 
 void setup() {
   //Initializes the RTC
@@ -27,49 +42,43 @@ void setup() {
   Serial.begin(9600);
   //Sets RTC time based on the PC time
   rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-  minEntryTime->insert("3 36 233 14", "16:00");
-  maxEntryTime->insert("3 36 233 14", "18:00");
-  strcpy(allowedIDs[0], "3 36 233 14");
+  for(int i = 0; i<111; i++){
+    EEPROM[i] = 0;
+  }
 }
 
 void loop() {
   //If I get tag ID from the other arduino
   if(ardvComm.available()>3){
+    int memIdEEPROM = 0;
     int p1 = ardvComm.read();
     int p2 = ardvComm.read();
     int p3 = ardvComm.read();
     int p4 = ardvComm.read();
     ardvComm.flush();
-    //Format ID form later work
-    sprintf(buffer,"%u %u %u %u", p1, p2, p3, p4);
     bool isAllowed = false;
-    Serial.println(buffer);
     //Chech if the ID is allowed
-    for(int i = 0; i < sizeof(allowedIDs)/sizeof(allowedIDs[0]); i++){
-      if (strcmp(allowedIDs[i], buffer) == 0) isAllowed = true;
+    for(int i = 0; i <= 90; i = i + 10){
+      if(EEPROM[i] == p1 && EEPROM[i+1] == p2 && EEPROM[i+2] == p3 && EEPROM[i+3] == p4){
+        isAllowed = true;
+        memIdEEPROM = i;
+      }     
     }
     //Checks if the user is allowed to enter
     if(isAllowed){
       //Now we need to check if the ID can enter at this time
       //Creates a DateTime object and set to it the current time
       DateTime now = rtc.now();
-      unsigned int maxHour;
-      unsigned int maxMin;
-      unsigned int minHour;
-      unsigned int minMin;
-      //Gets the minimal and maximal entry time of the specific ID and saves it into the specified vars
-      sscanf(minEntryTime->search(buffer).c_str(),"%u:%u",&minHour, &minMin);
-      sscanf(maxEntryTime->search(buffer).c_str(),"%u:%u",&maxHour, &maxMin);
       //If the time is obviously in the limit
-      if(minHour < now.hour() && maxHour > now.hour()){
+      if(EEPROM[memIdEEPROM+4] < now.hour() && EEPROM[memIdEEPROM+6] > now.hour()){
         answYes();
       }
       //If the time is on the lower border but still in
-      else if(minHour == now.hour() && now.minute() >= minMin){
+      else if(EEPROM[memIdEEPROM+4] == now.hour() && now.minute() >= EEPROM[memIdEEPROM+5]){
         answYes();
       }
       //If the time is on the uppper border but still in
-      else if(maxHour == now.hour() && now.minute() < maxMin){
+      else if(EEPROM[memIdEEPROM+6] == now.hour() && now.minute() < EEPROM[memIdEEPROM+7]){
         answYes();
       }
       //If the time isnt in the limit
@@ -83,8 +92,51 @@ void loop() {
   }
   //If there's a command from PC
   if(Serial.available()){
+    //THIS IS SLOW AS FUCK, BUT I CANT BE FUCKED TO WRITE MY OWN FUNCTION, SO THIS WILL DO :)
     String reqFromPC = Serial.readString();
+    //If i get the ADD command from PC
+    if(reqFromPC[0] == 'A'){
+      int avlID = -1;
+      //Finds the 1st free space in EEPROM
+      for(int i = 100; i <= 110; i++){
+        if(EEPROM[i] == 0) {
+          avlID = i;
+          break;
+        }
+      }
+      //If theres a free space in EEPROM
+      if(avlID != -1){
+        //Get the ID of wanted device and and set its max time as always
+        char ph;
+        int d1,d2,d3,d4;
+        Serial.println(reqFromPC.c_str());
+        sscanf(reqFromPC.c_str(),"A %i %i %i %i", &d1, &d2, &d3, &d4);
+        EEPROM[avlID-100] = d1;
+        EEPROM[avlID-99] = d2;
+        EEPROM[avlID-98] = d3;
+        EEPROM[avlID-97] = d4;
+        EEPROM[avlID-94] = 23;
+        EEPROM[avlID-93] = 59;
+      }else{
+        Serial.println("The maximum amount od IDs has already been registered.\nPlease remove one using the R commmand.");
+      }
+    }
+    //If i get the REMOVE command from PC
+    else if(reqFromPC[0] == 'R'){
 
+    }
+    //If i get the TIME command from PC
+    else if(reqFromPC[0] == 'T'){
+
+    }
+    //If i get the SHOW command from PC
+    else if(reqFromPC[0] == 'S'){
+
+    }
+    //If I get unknow command from PC
+    else{
+      Serial.println("Unknown command");
+    }
 
   }
 }
